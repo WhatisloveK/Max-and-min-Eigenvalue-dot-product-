efd96b70a0a2987873cc028ce2f8678edbed65b3
Каплюк Владислав Олегович <kuplykvlad@gmail.com>
2019-04-06 19:30:01 +0300

    Done

--- Eigenvalues/Form1.Designer.cs
+++ Eigenvalues/Form1.Designer.cs
@@ -28,13 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownRows = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownColumns = new System.Windows.Forms.NumericUpDown();
+            this.buttonSize = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxAccuracy = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelMax = new System.Windows.Forms.Label();
+            this.labelMin = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColumns)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 163);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(331, 275);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Size";
+            // 
+            // numericUpDownRows
+            // 
+            this.numericUpDownRows.Location = new System.Drawing.Point(82, 41);
+            this.numericUpDownRows.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownRows.Name = "numericUpDownRows";
+            this.numericUpDownRows.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDownRows.TabIndex = 2;
+            this.numericUpDownRows.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownColumns
+            // 
+            this.numericUpDownColumns.Location = new System.Drawing.Point(125, 41);
+            this.numericUpDownColumns.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownColumns.Name = "numericUpDownColumns";
+            this.numericUpDownColumns.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDownColumns.TabIndex = 3;
+            this.numericUpDownColumns.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // buttonSize
+            // 
+            this.buttonSize.Location = new System.Drawing.Point(168, 38);
+            this.buttonSize.Name = "buttonSize";
+            this.buttonSize.Size = new System.Drawing.Size(36, 23);
+            this.buttonSize.TabIndex = 4;
+            this.buttonSize.Text = "Aply";
+            this.buttonSize.UseVisualStyleBackColor = true;
+            this.buttonSize.Click += new System.EventHandler(this.buttonSize_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 67);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Find Eigenvalue";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxAccuracy
+            // 
+            this.textBoxAccuracy.Location = new System.Drawing.Point(82, 85);
+            this.textBoxAccuracy.Name = "textBoxAccuracy";
+            this.textBoxAccuracy.Size = new System.Drawing.Size(122, 20);
+            this.textBoxAccuracy.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Accuracy";
+            // 
+            // labelMax
+            // 
+            this.labelMax.AutoSize = true;
+            this.labelMax.Location = new System.Drawing.Point(12, 133);
+            this.labelMax.Name = "labelMax";
+            this.labelMax.Size = new System.Drawing.Size(0, 13);
+            this.labelMax.TabIndex = 8;
+            // 
+            // labelMin
+            // 
+            this.labelMin.AutoSize = true;
+            this.labelMin.Location = new System.Drawing.Point(179, 133);
+            this.labelMin.Name = "labelMin";
+            this.labelMin.Size = new System.Drawing.Size(0, 13);
+            this.labelMin.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(355, 450);
+            this.Controls.Add(this.labelMin);
+            this.Controls.Add(this.labelMax);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxAccuracy);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSize);
+            this.Controls.Add(this.numericUpDownColumns);
+            this.Controls.Add(this.numericUpDownRows);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColumns)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownRows;
+        private System.Windows.Forms.NumericUpDown numericUpDownColumns;
+        private System.Windows.Forms.Button buttonSize;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxAccuracy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelMax;
+        private System.Windows.Forms.Label labelMin;
     }
 }
 
--- Eigenvalues/Form1.cs
+++ Eigenvalues/Form1.cs
@@ -7,14 +7,132 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using MathNet.Numerics.LinearAlgebra;
 namespace Eigenvalues
 {
     public partial class Form1 : Form
     {
+        private Matrix<double> A,B,E;
+        private Vector<double> X, Xp,e;
+        private double eps;
         public Form1()
         {
             InitializeComponent();
+            InitializeDataGridView(2, 2);
+        }
+        private void InitializeDataGridView(int rows, int columns)
+        {
+            dataGridView1.ColumnCount = columns;
+            dataGridView1.ColumnHeadersVisible = true;
+
+            DataGridViewCellStyle columnHeaderStyle = new DataGridViewCellStyle();
+            columnHeaderStyle.BackColor = Color.Beige;
+            columnHeaderStyle.Font = new Font("Times New Roman", 12, FontStyle.Bold);
+            dataGridView1.ColumnHeadersDefaultCellStyle = columnHeaderStyle;
+
+
+            for (int i = 0; i < columns; ++i)
+            {
+                dataGridView1.Columns[i].Width = 64;
+                dataGridView1.Columns[i].Name = (i + 1).ToString();
+            }
+
+            dataGridView1.RowCount = rows + 2;
+            dataGridView1.RowHeadersVisible = true;
+
+            DataGridViewCellStyle rowHeaderStyle = new DataGridViewCellStyle();
+            rowHeaderStyle.BackColor = Color.Beige;
+            rowHeaderStyle.Font = new Font("Times New Roman", 12, FontStyle.Bold);
+            dataGridView1.RowHeadersDefaultCellStyle = rowHeaderStyle;
+
+            //f = new Class26BasedSys();
+            for (int i = 0; i < rows; ++i)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = (i + 1).ToString();
+                dataGridView1.Rows[i].ReadOnly = false;
+            }
+            dataGridView1.Rows[rows].HeaderCell.Value = "";
+            dataGridView1.Rows[rows].ReadOnly = true;
+            dataGridView1.Rows[rows + 1].HeaderCell.Value = "X0";
+
+            dataGridView1.AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders);
+            dataGridView1.RowHeadersWidth = 64;
+            numericUpDownColumns.Value = columns;
+            numericUpDownRows.Value = rows;
+        }
+
+        private void buttonSize_Click(object sender, EventArgs e)
+        {
+            InitializeDataGridView((int)numericUpDownRows.Value, (int)numericUpDownColumns.Value);
+        }
+
+        private void InitMatrics()
+        {
+            int rows = dataGridView1.RowCount - 2,
+               columns = dataGridView1.ColumnCount;
+
+            double[,] a = new double[rows, columns];
+            double[,] b = new double[rows, columns];
+            double[] v = new double[columns];
+
+            for (int i = 0; i < rows-1; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    a[i, j] = Double.Parse(dataGridView1.Rows[i].Cells[j].Value.ToString());
+                    b[i, j] = 0;
+                }
+                b[i, i] = 1;
+                v[i] = Double.Parse(dataGridView1.Rows[rows].Cells[i].Value.ToString());
+            }
+            Xp = Vector<double>.Build.Dense(columns, (i) => v[i]);
+            A = Matrix<double>.Build.Dense(rows - 1, columns, (i, j) => a[i, j]);
+            E = Matrix<double>.Build.Dense(rows - 1, columns, (i, j) => b[i, j]);
+        }
+
+        private double MaxEigenvalue(Matrix<double>A_, Vector<double> xp, ref Vector<double>x, ref Vector<double>e_)
+        {
+            double u, up;
+
+            e = xp / xp.Norm(2);
+            x = A_ * e;
+            u = x.DotProduct(e);
+
+            do
+            {
+                xp = x;
+                up = u;
+                e = xp / xp.Norm(2);
+                x = A_ * e;
+                u = x.DotProduct(e);
+            }
+            while (Math.Abs(u - up) > eps);
+
+            return u;
+        }
+
+        private void Execute()
+        {
+            InitMatrics();
+            double l_B, l_A = MaxEigenvalue(A, Xp, ref X, ref e);
+            B = l_A * E - A;
+            if (A.IsSymmetric())
+            {
+                l_B = MaxEigenvalue(B, Xp, ref X, ref e);
+                labelMin.Text = "Min eigenvalue : " + (l_A - l_B);
+            }
+
+            labelMax.Text = "Max eigenvalue : " + l_A;
+        } 
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(textBoxAccuracy.Text, out eps))
+                MessageBox.Show("Enter correct Acuracy");
+            else
+                Execute();
+            
         }
     }
 }
